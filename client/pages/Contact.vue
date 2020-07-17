4733ee4d86f07a7d77632745dc6328c0ff88a3f5
Jed <valentin.jeudy@gmail.com>
2020-07-17 16:03:10 +0200

    add contact page

---
+++ client/pages/Contact.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="page-content">
+    <h1>Contact</h1>
+    <p>Do you have any question / proposition ?</p>
+    <p>You can send me a message here !</p>
+    <form action="">
+      <label for="">Subject</label>
+      <input type="text">
+      <label for="">Mail</label>
+      <input type="text">
+      <label for="">Message</label>
+      <textarea id="" name="" cols="30" rows="10" />
+      <button>Send</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style src="./Contact.scss" scoped lang="scss" />
