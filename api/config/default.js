48a877ddf9263b96e838ae8b0fe26826384257c1
Jed <valentin.jeudy@gmail.com>
2020-07-21 17:10:53 +0200

    add global package

--- api/config/default.js
+++ api/config/default.js
@@ -11,7 +11,7 @@ module.exports = {
     }
   },
   corsOptions: {
-    origin: 'http://localhost:3000',
+    origin: '*',
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
   },
   jwtConfig: {
