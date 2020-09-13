14d4a62ed1b2e7e52866c6ba7b6c1cb17aa9865d
ValJed <vjeudy@protonmail.com>
2020-09-13 17:43:39 +0000

    add ecosystem config for pm2

---
+++ api/ecosystem.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  apps : [{
+	  name: 'portfolio-api',
+    script: 'npm run start',
+    watch: false,
+	  autorestart: false,
+    instance_var: 'INSTANCE_ID'
+  }],
+  deploy : {
+    production : {
+      user : 'SSH_USERNAME',
+      host : 'SSH_HOSTMACHINE',
+      ref  : 'origin/master',
+      repo : 'GIT_REPOSITORY',
+      path : 'DESTINATION_PATH',
+      'pre-deploy-local': '',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': ''
+    }
+  }
+};
